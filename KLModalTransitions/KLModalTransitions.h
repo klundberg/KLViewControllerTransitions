cbd67164d599dbd5e70157a73054c715ffa08890
Kevin Lundberg <kevinrlundberg@gmail.com>
2013-09-26 08:48:00 -0400

    Initial Commit

---
+++ KLModalTransitions/KLModalTransitions.h
@@ -0,0 +1,13 @@
+//
+//  KLModalTransitions.h
+//  KLModalTransitions
+//
+//  Created by Kevin Lundberg on 9/26/13.
+//  Copyright (c) 2013 Lundbergsoft. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KLModalTransitions : NSObject
+
+@end
---
+++ KLModalTransitions/KLModalTransitions.m
@@ -0,0 +1,13 @@
+//
+//  KLModalTransitions.m
+//  KLModalTransitions
+//
+//  Created by Kevin Lundberg on 9/26/13.
+//  Copyright (c) 2013 Lundbergsoft. All rights reserved.
+//
+
+#import "KLModalTransitions.h"
+
+@implementation KLModalTransitions
+
+@end
